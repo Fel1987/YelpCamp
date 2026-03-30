7e207e49027e37c5f0bc4e65235ace5403169be1
Felipe A Walton A <felwal1987@gmail.com>
2026-03-30 17:29:46 -0600

    Added: Bootstrap Client-Side Validation, Basic Server-side Validation Utilities.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utilities/ExpressError");
+const catchAsync = require("./utilities/catchAsync");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -27,57 +29,76 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-
-  res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  }),
+);
 
 app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-  const newCampground = new Campground(req.body.campground);
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+  }),
+);
 
-  await newCampground.save();
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const foundCampground = await Campground.findById(id);
+    res.render("campgrounds/show", { foundCampground });
+  }),
+);
 
-  res.redirect(`/campgrounds/${newCampground._id}`);
-});
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const foundCampground = await Campground.findById(id);
+    res.render("campgrounds/edit", { foundCampground });
+  }),
+);
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const foundCampground = await Campground.findById(id);
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const editedCampground = await Campground.findByIdAndUpdate(
+      id,
+      { ...req.body.campground },
+      {
+        runValidators: true,
+        new: true,
+      },
+    );
 
-  res.render("campgrounds/show", { foundCampground });
-});
+    res.redirect(`/campgrounds/${id}`);
+  }),
+);
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const foundCampground = await Campground.findById(id);
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const deletedCampground = await Campground.findByIdAndDelete(id);
 
-  res.render("campgrounds/edit", { foundCampground });
-});
+    res.redirect("/campgrounds");
+  }),
+);
 
-app.put("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const editedCampground = await Campground.findByIdAndUpdate(
-    id,
-    { ...req.body.campground },
-    {
-      runValidators: true,
-      new: true,
-    },
-  );
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
 
-  res.redirect(`/campgrounds/${id}`);
-});
-
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedCampground = await Campground.findByIdAndDelete(id);
-
-  res.redirect("/campgrounds");
+  res.send("Something went wrong");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
